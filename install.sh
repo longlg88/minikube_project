49c467288cdc05e7bb0b816c7ee157faeb98a3ca
longlg88 <longlg88@naver.com>
2019-12-16 07:58:22 +0000

    Add chown

--- install.sh
+++ install.sh
@@ -68,6 +68,11 @@ sudo apt-get -y install kubectl
 echo -e "minikube download"
 sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && mv minikube /usr/bin/
 
+sudo chown $USER:$USER -R ~/.kube
+sudo chown $USER:$USER -R ~/.minikube
+sudo usermod -a -G docker $USER
+sudo service docker restart
+
 echo "alias k=kubectl" >> ~/.bashrc
 
 
