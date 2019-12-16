e8067febc0dde3774b51080ee8bde891c03a329a
longlg88 <longlg88@naver.com>
2019-12-16 06:58:51 +0000

    Test sudo

--- install.sh
+++ install.sh
@@ -14,42 +14,42 @@ popd(){
 
 # update
 echo -e "apt update"
-apt update
+sudo apt update
 # install packages
 echo -e "install packages"
-apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get -y install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 
 # add docker official gpg key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 # signing
 echo -e "signing for docker"
-apt-key fingerprint 0EBFCD88
+sudo apt-key fingerprint 0EBFCD88
 # add repository docker
 echo -e "add repository docker"
 if [[ -n `grep -r "download.docker.com" /etc/apt/sources.list` ]]; then
     echo -e "here"
 else
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 fi
 
 # one more update
 echo -e "one more update"
-apt update
+sudo apt update
 # install docker-ce
 echo -e "install docker-ce"
-apt-get -y install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 # add repository kubernetes
 echo -e "add repository kubernetes"
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
 if [[ -n `grep -r "kubernetes-xenial" /etc/apt/sources.list.d/` ]]; then
     echo -e "here"
 else
     # touch repository kubernetes
     echo -e "touch repository kubernetes"
-    touch /etc/apt/sources.list.d/kubernetes.list
+    sudo touch /etc/apt/sources.list.d/kubernetes.list
 
     # write kubernetes.list
     echo -e "write kubernetes.list"
@@ -58,15 +58,15 @@ fi
 
 # one more update
 echo -e "one more update"
-apt update
+sudo apt update
 
 # install kubectl
 echo -e "install kubectl"
-apt-get -y install kubectl
+sudo apt-get -y install kubectl
 
 # minikube download
 echo -e "minikube download"
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && mv minikube /usr/bin/
+sudo curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube && mv minikube /usr/bin/
 
 echo "alias k=kubectl" >> ~/.bashrc
 
